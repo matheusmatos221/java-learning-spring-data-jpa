ecc0b16845903f064ba0f75b6118556fe2bf4d8a
Matheus Matos <matheus.matos@accurate.com.br>
2022-03-16 08:22:31 -0300

    UPDATE SpringDataApplication - ADD a CargoRepository dependency injection and ADD a public void run setting a new Cargo with anyway description and uses repository.save to persists the new Cargo on current table.

--- spring-data/src/main/java/br/com/matheus/spring/data/SpringDataApplication.java
+++ spring-data/src/main/java/br/com/matheus/spring/data/SpringDataApplication.java
@@ -1,13 +1,27 @@
 package br.com.matheus.spring.data;
 
+import br.com.matheus.spring.data.orm.Cargo;
+import br.com.matheus.spring.data.repository.CargoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+	private final CargoRepository repository;
+
+	public SpringDataApplication(CargoRepository repository){
+		this.repository = repository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+		repository.save(cargo);
+	}
 }
