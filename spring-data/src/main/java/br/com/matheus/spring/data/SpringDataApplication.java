99d228ddb163c8e21c54700a0292a63a5806730f
Matheus Matos <matheus.matos@accurate.com.br>
2022-03-29 00:56:07 -0300

    UPDATE mudou o banco para mySQL, atualiza toString da entidade Cargo, adiciona Serviço de CRUD para cargo, adiciona scanner para ler inputs no inicio da aplicação

--- spring-data/src/main/java/br/com/matheus/spring/data/SpringDataApplication.java
+++ spring-data/src/main/java/br/com/matheus/spring/data/SpringDataApplication.java
@@ -1,17 +1,19 @@
 package br.com.matheus.spring.data;
 
-import br.com.matheus.spring.data.orm.Cargo;
-import br.com.matheus.spring.data.repository.CargoRepository;
+import br.com.matheus.spring.data.service.CrudCargoService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
-	private final CargoRepository repository;
+	private final CrudCargoService cargoService;
+	private boolean system = true;
 
-	public SpringDataApplication(CargoRepository repository){
-		this.repository = repository;
+	public SpringDataApplication(CrudCargoService cargoService){
+		this.cargoService = cargoService;
 	}
 
 	public static void main(String[] args) {
@@ -20,8 +22,19 @@ public class SpringDataApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) {
-		Cargo cargo = new Cargo();
-		cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
-		repository.save(cargo);
+		Scanner scanner = new Scanner(System.in);
+		while(system){
+			System.out.println("Qual ação você quer executar? ");
+			System.out.println("0 - Sair");
+			System.out.println("1 - Cargo");
+
+			int action = scanner.nextInt();
+			if(action==1){
+				cargoService.inicial(scanner);
+			}
+			else{
+				this.system = false;
+			}
+		}
 	}
 }
---
+++ spring-data/src/main/java/br/com/matheus/spring/data/service/CrudCargoService.java
@@ -0,0 +1,88 @@
+package br.com.matheus.spring.data.service;
+
+import br.com.matheus.spring.data.orm.Cargo;
+import br.com.matheus.spring.data.repository.CargoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service
+public class CrudCargoService {
+    private final CargoRepository cargoRepository;
+    private boolean system = true;
+
+    public CrudCargoService(CargoRepository cargoRepository){
+        this.cargoRepository = cargoRepository;
+    }
+
+    public void inicial(Scanner scanner){
+        while(system){
+            System.out.println("Qual acao de cargo deseja executar?");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Salvar");
+            System.out.println("2 - Atualizar");
+            System.out.println("3 - Visualizar");
+            System.out.println("4 - Deletar");
+
+            int action = scanner.nextInt();
+
+            switch(action){
+                case 0:
+                    system = false;
+                    break;
+                case 1:
+                    salvar(scanner);
+                    break;
+                case 2:
+                    atualizar(scanner);
+                    break;
+                case 3:
+                    visualizar();
+                    break;
+                case 4:
+                    deletar(scanner);
+                    break;
+                default:
+                    System.out.println("Você selecionou uma opção inválida!");
+                    system = false;
+                    break;
+            }
+        }
+    }
+    private void salvar(Scanner scanner){
+        System.out.println("Descricao do cargo");
+        String descricao = scanner.next();
+        Cargo cargo = new Cargo();
+        cargo.setDescricao(descricao);
+        cargoRepository.save(cargo);
+        System.out.println("Salvo");
+    }
+
+    private void atualizar(Scanner scanner){
+        System.out.println("Digite o ID do Cargo");
+        Integer id = scanner.nextInt();
+        System.out.println("Digite a nova descricao");
+        String descricao = scanner.next();
+
+        Cargo cargo = new Cargo();
+        cargo.setId(id);
+        cargo.setDescricao(descricao);
+
+        cargoRepository.save(cargo);
+        System.out.println("Atualizado!");
+    }
+
+    private void visualizar(){
+        Iterable<Cargo> cargos = cargoRepository.findAll();
+        cargos.forEach(System.out::println);
+    }
+
+
+    public void deletar(Scanner scanner){
+        System.out.println("Id");
+        Integer id = scanner.nextInt();
+        cargoRepository.deleteById(id);
+        System.out.println("Deletado");
+    }
+
+}
